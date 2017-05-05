f9b533be9c81f309c5a8410f529a8302ac3b8250
yuweitu <yuvi_tu@outlook.com>
2017-05-05 18:04:29 -0400

    Time Related

---
+++ PartTwo_TimeSeriesAnalysis.py
@@ -0,0 +1,150 @@
+import plotly.tools as tls
+tls.set_credentials_file(username='gaolingshan', api_key='VtYTYiUcXzZcQ3xM5hzU')
+#Note that the key may be invalid when re-run the program
+
+
+import pandas as pd
+import numpy as np
+from IPython.display import display
+
+import plotly.plotly as py
+import plotly.graph_objs as go# interactive graphing
+from plotly.graph_objs import Bar, Scatter, Marker, Layout 
+from datetime import datetime
+from scipy import stats  
+
+import holidays
+us_holidays = holidays.UnitedStates()
+
+df = pd.read_csv("Cleaned_data_updated_zipcode.csv",usecols = [0,1,2,3,4,5])
+new_df = pd.DataFrame(df['CMPLNT_NUM'].groupby(df['CMPLNT_FR_DT']).count().reset_index())
+
+#transfor to DateTime type and add weekday/weekend, holiday/non holiday information
+
+us_holidays = holidays.UnitedStates()
+
+for i in range(len(new_df)):
+    try:
+        new_df.ix[i,'CMPLNT_FR_DT'] = datetime.strptime(new_df.ix[i,'CMPLNT_FR_DT'],"%m/%d/%Y")
+        new_df.ix[i,'YEAR'] = new_df.ix[i,'CMPLNT_FR_DT'].year
+        new_df.ix[i,'MONTH'] = new_df.ix[i,'CMPLNT_FR_DT'].month
+        new_df.ix[i,'DAY'] = new_df.ix[i,'CMPLNT_FR_DT'].day
+        new_df.ix[i,'If_Public_Holiday'] = new_df.ix[i,'CMPLNT_FR_DT'] in us_holidays
+        new_df.ix[i,'WEEKDAY'] = new_df.ix[i,'CMPLNT_FR_DT'].weekday()
+    except:
+        print(new_df.ix[i,'CMPLNT_FR_DT'])
+
+
+
+#only analyze from 2005 Jan to 2015 Dec
+new_df = new_df[(new_df['YEAR'] < 2016) & (new_df['YEAR'] > 2005)]
+
+
+# visualize the number of crimes per year
+df_year = pd.DataFrame(new_df['CMPLNT_NUM'].groupby(new_df['YEAR']).sum().reset_index())
+year = go.Scatter(
+    x=df_year['YEAR'],
+    y=df_year['CMPLNT_NUM'],
+    name='CMPLNT_NUM per year'
+)
+
+data = [year]
+layout = go.Layout(
+    title='CMPLNT_NUM per year')
+fig = go.Figure(data=data, layout=layout)
+py.image.save_as(fig, filename='CMPLNT_NUM per year.png')
+
+
+# visualize the number of crimes per month
+df_month = pd.DataFrame(new_df['CMPLNT_NUM'].groupby(new_df['MONTH']).sum().reset_index())
+month = go.Scatter(
+    x=df_month['MONTH'],
+    y=df_month['CMPLNT_NUM'],
+    name='CMPLNT_NUM per month'
+)
+
+data = [month]
+layout = go.Layout(
+    title='CMPLNT_NUM per month')
+fig = go.Figure(data=data, layout=layout)
+py.image.save_as(fig, filename='CMPLNT_NUM per month.png')
+
+
+# visualize the number of crimes per monthly day
+df_day = pd.DataFrame(new_df['CMPLNT_NUM'].groupby(new_df['DAY']).sum().reset_index())
+day = go.Scatter(
+    x=df_day['DAY'],
+    y=df_day['CMPLNT_NUM'],
+    name='CMPLNT_NUM per day'
+)
+
+data = [day]
+layout = go.Layout(
+    title='CMPLNT_NUM per day')
+fig = go.Figure(data=data, layout=layout)
+py.image.save_as(fig, filename='CMPLNT_NUM per day.png')
+
+
+
+# visualize the number of crimes per weekday
+df_weekday = pd.DataFrame(new_df['CMPLNT_NUM'].groupby(new_df['WEEKDAY']).sum().reset_index())
+df_weekday['WEEKDAY'] = ['MON', 'TUE','WED','THU','FRI','SAT','SUN']
+weekday = go.Bar(
+    x=df_weekday['WEEKDAY'],
+    y=df_weekday['CMPLNT_NUM'],
+    name='CMPLNT_NUM per weekday',
+    marker=dict(
+        color=['rgba(204,204,204,1)', 'rgba(204,204,204,1)','rgba(204,204,204,1)',
+               'rgba(204,204,204,1)', 'rgba(204,204,204,1)',
+               'rgba(222,45,38,0.8)','rgba(222,45,38,0.8)']),
+)
+
+data = [weekday]
+layout = go.Layout(
+    title='CMPLNT_NUM per weekday')
+fig = go.Figure(data=data, layout=layout)
+py.image.save_as(fig, filename='CMPLNT_NUM per weekday.png')
+
+
+#calculate correlation coefficients
+weekday  = new_df['CMPLNT_NUM'].groupby([new_df['WEEKDAY'],new_df['YEAR']]).sum().unstack().reset_index()
+weekday['If_Weekend'] = weekday['WEEKDAY'].map(lambda x: x ==5 or x ==6)
+weekday = weekday.groupby(weekday['If_Weekend']).mean().reset_index()
+week = [weekday.ix[0,i] for i in range(3,12)]
+weekend = [weekday.ix[1,i] for i in range(3,12)]
+
+stat,pvalue = stats.ttest_ind(week, weekend)
+print("The p-value for this ttest is", pvalue)
+
+
+#visualize th number of crimes per day in 2015 including holiday/non-holiday
+new_df_2015 = new_df[new_df['YEAR'] == 2015].reset_index()
+
+color_list = []
+for i in range(len(new_df_2015)):
+    if new_df_2015.ix[i,'If_Public_Holiday'] is True:
+        color_list.append('rgba(222,45,38,0.8)')
+    else:
+        color_list.append('rgba(204,204,204,1)')
+        
+plot_2015 = go.Bar(
+    x=new_df_2015['CMPLNT_FR_DT'],
+    y=new_df_2015['CMPLNT_NUM'],
+    name='CMPLNT_NUM per day in 2015(Holiday)',
+    marker=dict(
+        color=color_list),
+)
+data = [plot_2015]
+layout = go.Layout(
+    title='CMPLNT_NUM per day in 2015(Holiday)')
+fig = go.Figure(data=data, layout=layout)
+py.iplot(fig)
+py.image.save_as(fig, filename='CMPLNT_NUM per day in 2015(Holiday).png')
+
+#calculate correlation coefficients
+holiday = new_df['CMPLNT_NUM'].groupby([new_df['If_Public_Holiday'],new_df['YEAR']]).mean().unstack().reset_index()
+holiday_mean = [holiday.ix[0,i] for i in range(2,11)]
+non_holiday_mean = [holiday.ix[1,i] for i in range(2,11)]
+stat,pvalue = stats.ttest_ind(holiday_mean, non_holiday_mean)
+print("The p-value for this ttest is", pvalue)
+
---
+++ PartTwo_TimeSeriesWithExternalData.py
@@ -0,0 +1,247 @@
+import plotly.tools as tls
+tls.set_credentials_file(username='gaolingshan', api_key='VtYTYiUcXzZcQ3xM5hzU')
+#Note that the key may be invalid when re-run the program
+
+import pandas as pd
+import datetime as dt
+from IPython.display import display
+
+import plotly.plotly as py
+import plotly.graph_objs as go# interactive graphing
+from plotly.graph_objs import Bar, Scatter, Marker, Layout 
+from datetime import datetime
+
+#Please remind to keep program and external data files in the same folder
+#read Condo Price Index Data/Labor Data
+condo = pd.read_csv("NYC_Condo_Price_Index.csv").dropna()
+labor = pd.read_csv("NYC_LaborForce_Data.csv").dropna()
+
+#transfor into DateTime type
+for i in range(len(condo)):
+    condo.ix[i,'DATE'] = datetime.strptime(condo.ix[i,'DATE'],"%Y-%m-%d")
+    condo.ix[i,'YEAR'] = condo.ix[i,'DATE'].year
+    condo.ix[i,'MONTH'] = condo.ix[i,'DATE'].month
+
+for i in range(len(labor)):
+        labor.ix[i,'DATE'] = datetime.strptime(labor.ix[i,'Month'],"%b-%y")
+        labor.ix[i,'YEAR'] = labor.ix[i,'DATE'].year
+        labor.ix[i,'MONTH'] = labor.ix[i,'DATE'].month
+
+
+#analyze data from 2005 Jan to 2015 Dec
+condo = condo[(condo['YEAR'] < 2016) & (condo['YEAR'] > 2005)].reset_index()
+labor = labor[(labor['YEAR'] < 2016) & (labor['YEAR'] > 2005)].reset_index()
+
+
+
+#read original data
+df = pd.read_csv("Cleaned_data_updated_zipcode.csv",usecols = [0,1,2,3,4,5])
+#group by date
+new_df = pd.DataFrame(df['CMPLNT_NUM'].groupby(df['CMPLNT_FR_DT']).count().reset_index())
+
+#transform DateTime type
+for i in range(len(new_df)):
+    try:
+        new_df.ix[i,'CMPLNT_FR_DT'] = datetime.strptime(new_df.ix[i,'CMPLNT_FR_DT'],"%m/%d/%Y")
+        new_df.ix[i,'YEAR'] = new_df.ix[i,'CMPLNT_FR_DT'].year
+        new_df.ix[i,'MONTH'] = new_df.ix[i,'CMPLNT_FR_DT'].month
+    except:
+        print(new_df.ix[i,'CMPLNT_FR_DT'])
+
+# group by(year,month) to prepare for merging
+crime_time = new_df['CMPLNT_NUM'].groupby([new_df['YEAR'],new_df['MONTH']]).sum().reset_index()
+crime_time = crime_time[(crime_time['YEAR'] < 2016)&(crime_time['YEAR'] >2005)].reset_index()
+
+#merge external dataset with original data
+condo_with_crime = pd.concat([condo, crime_time['CMPLNT_NUM']], axis=1)
+labor_with_crime = pd.concat([labor, crime_time['CMPLNT_NUM']], axis=1)
+
+'''
+Analyze the relationship between Condo Price Index and Number of Crimes
+'''
+
+#calculate monthly correlation coefficients
+print('-------------------------------------------------')
+print('The correlation coefficient between Number of Crimes per month and Conde Price Index per month is: \n')
+print(condo_with_crime['NYXRCSA'].corr(condo_with_crime['CMPLNT_NUM']))
+print('-------------------------------------------------')
+print('The correlation coefficient between Number of Crimes per month and Unemployment Rate per month is: \n')
+print(labor_with_crime['Unemp Rate(%)'].corr(condo_with_crime['CMPLNT_NUM']))
+print('-------------------------------------------------')
+print('The correlation coefficient  between Number of Crimes per month and Labor Force Participation Rate per month is: \n')
+print(labor_with_crime['LFPART(%)'].corr(condo_with_crime['CMPLNT_NUM']))
+print('-------------------------------------------------')
+print('The correlation coefficient  between Number of Crimes per month and Employment/Population ratio  per month is: \n')
+print(labor_with_crime['Emp/PopRatio (%)'].corr(condo_with_crime['CMPLNT_NUM']))
+print('-------------------------------------------------')
+
+#plotting
+# visualize the relationship between the number of crimes and condo price index per month
+# from 2005 Jan to 2015 Dec
+trace1 = go.Scatter(
+    x=condo_with_crime['DATE'],
+    y=condo_with_crime['NYXRCSA'],
+    name='Conde Price Index'
+)
+trace2 = go.Scatter(
+    x=condo_with_crime['DATE'],
+    y=condo_with_crime['CMPLNT_NUM'],
+    name='COMPLNT_NUM',
+    yaxis='y2'
+)
+data = [trace1, trace2]
+layout = go.Layout(
+    title='CMPLNT_NUM vs Condo Price Index',
+    yaxis=dict(
+        title='Condo Price Index'
+    ),
+    yaxis2=dict(
+        title='Number of Crimes per month',
+        titlefont=dict(
+            color='rgb(148, 103, 189)'
+        ),
+        tickfont=dict(
+            color='rgb(148, 103, 189)'
+        ),
+        overlaying='y',
+        side='right'
+    )
+)
+fig = go.Figure(data=data, layout=layout)
+py.image.save_as(fig, filename='CMPLNT_NUM vs Condo Price Index.png')
+
+
+
+
+# visualize the relationship between the number of crimes and unemployemnt rate per month
+# from 2005 Jan to 2015 Dec
+trace1 = go.Scatter(
+    x=labor_with_crime['DATE'],
+    y=labor_with_crime['Unemp Rate(%)'],
+    name='Unemployment Rate(%)'
+)
+trace2 = go.Scatter(
+    x=condo_with_crime['DATE'],
+    y=condo_with_crime['CMPLNT_NUM'],
+    name='COMPLNT_NUM',
+    yaxis='y2'
+)
+data = [trace1, trace2]
+layout = go.Layout(
+    title='CMPLNT_NUM vs Unemployment Rate(%)',
+    yaxis=dict(
+        title='Unemployment Rate(%)'
+    ),
+    yaxis2=dict(
+        title='Number of Crimes per month',
+        titlefont=dict(
+            color='rgb(148, 103, 189)'
+        ),
+        tickfont=dict(
+            color='rgb(148, 103, 189)'
+        ),
+        overlaying='y',
+        side='right'
+    )
+)
+fig = go.Figure(data=data, layout=layout)
+py.image.save_as(fig, filename='CMPLNT_NUM vs Unemployment Rate(%).png')
+
+
+
+
+# visualize the relationship between the number of crimes and labor participation rate per month
+# from 2005 Jan to 2015 Dec
+trace1 = go.Scatter(
+    x=labor_with_crime['DATE'],
+    y=labor_with_crime['LFPART(%)'],
+    name='Labor Participation Rate'
+)
+trace2 = go.Scatter(
+    x=condo_with_crime['DATE'],
+    y=condo_with_crime['CMPLNT_NUM'],
+    name='COMPLNT_NUM',
+    yaxis='y2'
+)
+data = [trace1, trace2]
+layout = go.Layout(
+    title='CMPLNT_NUM vs Labor Participation Rate',
+    yaxis=dict(
+        title='Labor Participation Rate'
+    ),
+    yaxis2=dict(
+        title='Number of Crimes per month',
+        titlefont=dict(
+            color='rgb(148, 103, 189)'
+        ),
+        tickfont=dict(
+            color='rgb(148, 103, 189)'
+        ),
+        overlaying='y',
+        side='right'
+    )
+)
+fig = go.Figure(data=data, layout=layout)
+py.image.save_as(fig, filename='CMPLNT_NUM vs Labor Participation Rate.png')
+
+
+
+# visualize the relationship between the number of crimes and employment/population ratio per month
+# from 2005 Jan to 2015 Dec
+trace1 = go.Scatter(
+    x=labor_with_crime['DATE'],
+    y=labor_with_crime['Emp/PopRatio (%)'],
+    name='Employment/Population ratio'
+)
+trace2 = go.Scatter(
+    x=condo_with_crime['DATE'],
+    y=condo_with_crime['CMPLNT_NUM'],
+    name='COMPLNT_NUM',
+    yaxis='y2'
+)
+data = [trace1, trace2]
+layout = go.Layout(
+    title='CMPLNT_NUM vs Employment/Population ratio',
+    yaxis=dict(
+        title='Employment/Population ratio'
+    ),
+    yaxis2=dict(
+        title='Number of Crimes per month',
+        titlefont=dict(
+            color='rgb(148, 103, 189)'
+        ),
+        tickfont=dict(
+            color='rgb(148, 103, 189)'
+        ),
+        overlaying='y',
+        side='right'
+    )
+)
+fig = go.Figure(data=data, layout=layout)
+py.image.save_as(fig,filename = "CMPLNT_NUM vs Emp_Pop ratio.png.png")
+
+
+#calculate yearly correlation coefficients
+avg_condo = condo_with_crime['NYXRCSA'].groupby(condo_with_crime['YEAR']).mean()
+avg_unemp = labor_with_crime['Unemp Rate(%)'].groupby(labor_with_crime['YEAR']).mean()
+avg_lfpart = labor_with_crime['LFPART(%)'].groupby(labor_with_crime['YEAR']).mean()
+avg_emp_pop = labor_with_crime['Emp/PopRatio (%)'].groupby(labor_with_crime['YEAR']).mean()
+crime_sum = condo_with_crime['CMPLNT_NUM'].groupby(condo_with_crime['YEAR']).sum()
+
+
+print('-------------------------------------------------')
+print('The correlation coefficient between Number of Crimes per year and average Conde Price Index per year is: \n')
+print(avg_condo.corr(crime_sum))
+print('-------------------------------------------------')
+print('The correlation coefficient between Number of Crimes per year and average Unemployment Rate per year is: \n')
+print(avg_unemp.corr(crime_sum))
+print('-------------------------------------------------')
+print('The correlation coefficient  between Number of Crimes per year and average Labor Force Participation Rate per year is: \n')
+print(avg_lfpart.corr(crime_sum))
+print('-------------------------------------------------')
+print('The correlation coefficient  between Number of Crimes per year and average Employment/Population ratio  per year is: \n')
+print(avg_emp_pop.corr(crime_sum))
+print('-------------------------------------------------')
+
+
+
